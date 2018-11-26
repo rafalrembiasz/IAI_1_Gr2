d5506109371b5e5416e770039eb8e6f3688013c7
Przemek <przemekcichon97@gmail.com>
2018-11-26 19:01:47 +0100

    kolokwium1

---
+++ navbar.js
@@ -0,0 +1,19 @@
+let nav = '<div>\n' +
+    '\n' +
+    '<ul>\n' +
+    '    <li><a href="index.html">Index<a/></li>\n' +
+    '    <li><a href="blog.html">Blog<a/></li>\n' +
+    '</ul>\n' +
+    '\n' +
+    '</div>' ;
+
+
+
+let navbar = document.querySelector('.navbar');
+
+navbar.innerHTML = nav;
+
+
+
+
+
